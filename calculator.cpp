6ca2d34d37e6ebbb50501f687817d482bb8e0210
athomft <atho.mft@gmail.com>
2023-10-13 13:00:31 +0700

    Removing using namespace std; And and quit at the begining

--- calculator.cpp
+++ calculator.cpp
@@ -1,68 +1,72 @@
 #include <iostream>
 
-using namespace std;
-
 int a, b, c, op;
 char restart;
 
 void ask()
 {
-    cout << "Enter the first number: ";
-    cin >> a;
-    cout << "Enter the second number: ";
-    cin >> b;
+    std::cout << "Enter the first number: ";
+    std::cin >> a;
+    std::cout << "Enter the second number: ";
+    std::cin >> b;
 }
 
 int main()
 {
-    cout << "1. Multiplication\n";
-    cout << "2. Division\n";
-    cout << "3. Addition\n";
-    cout << "4. Subtraction\n";
+    std::cout << "1. Multiplication\n";
+    std::cout << "2. Division\n";
+    std::cout << "3. Addition\n";
+    std::cout << "4. Subtraction\n";
+    std::cout << "5. Quit\n";
 
-    cout << "Choose the operator (1/2/3/4): ";
-    cin >> op;
+    std::cout << "Choose the operator (1/2/3/4) or 5 to quit: ";
+    std::cin >> op;
 
     if (op == 1)
     {
         ask();
         c = a * b;
-        cout << "The result of " << a << " times " << b << " is " << c;
+        std::cout << "The result of " << a << " times " << b << " is " << c;
     }
     else if (op == 2)
     {
         ask();
         if (b == 0)
         {
-            cout << "Cannot be divided by 0";
+            std::cout << "Cannot be divided by 0";
             main();
         }
         else
         {
             c = a / b;
         }
-        cout << "The result of " << a << " divided by " << b << " is " << c;
+        std::cout << "The result of " << a << " divided by " << b << " is " << c;
     }
     else if (op == 3)
     {
         ask();
         c = a + b;
-        cout << "The result of " << a << " plus " << b << " is " << c;
+        std::cout << "The result of " << a << " plus " << b << " is " << c;
     }
     else if (op == 4)
     {
         ask();
         c = a / b;
-        cout << "The result of " << a << " minus " << b << " is " << c;
+        std::cout << "The result of " << a << " minus " << b << " is " << c;
     }
+    else if (op == 5)
+    {
+        exit(0);
+    }
+
     else
     {
-        cout << "Invalid operation\n\n";
+        std::cout << "Invalid operation\n\n";
         main();
     }
 
-    cout << "\n\n Do you want to restart the program (Y/N)? ";
-    cin >> restart;
+    std::cout << "\n\n Do you want to restart the program (Y/N)? ";
+    std::cin >> restart;
     if (restart == 'Y' || restart == 'y')
     {
         main();
